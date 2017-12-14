8100c73f4b39a2b5fdfdbdec6c4322f0ed105be8
Morgan Burrows <burrowmt@gmail.com>
2017-12-14 11:51:41 -0700

    commit 3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const styles = {
 const App = () => (
   <div style={styles}>
     <Hello name="Team ThunderCats" />
-    <h2>This is our kickstarter tesssttttt {'\u2728'}</h2>
+    <h2>This is our kickstarter tesssttttt test 2 {'\u2728'}</h2>
   </div>
 );
 
