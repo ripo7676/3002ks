ab43e5b6a96c4bc1cb2b5cc1b4a71f4e527afe13
Morgan Burrows <burrowmt@gmail.com>
2017-12-14 11:43:49 -0700

    this is morgans commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
+    <Hello name="Team ThunderCats" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
   </div>
 );
